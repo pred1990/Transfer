53fb3302c1db2e81367a10ff8a1c9878dfe90481
msk <steffenkin@hotmail.de>
2015-08-15 11:18:31 +0200

    Einfache eingabe implementiert

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,55 @@
 #include "Main"
-int main(int length, char* argv[])
+int main(int length, char** argv)
 {
+	
+	Transfer::TransferRequest request = Transfer::getTransferRequest(argv, (size_t)length);
+
+	if(Transfer::isEmpty(request))
+	{
+		request = Transfer::getTransferRequest(&std::cin);
+	}
 
 	std::cout << "This is a Simple Test please enter a number and the porgramm will exit" << std::endl;
 	int a = 0;
 	std::cin >> a; 
 }
+
+/* 
+ *	Reades the parameter of the programm and creates a TransferRequest 
+*/
+Transfer::TransferRequest Transfer::getTransferRequest(char** input, size_t length)
+{
+	Transfer::TransferRequest request = {}; 
+
+	//check empty parameter
+	if(length <= 0)
+		return request;
+
+	//start parsing 
+	std::stringstream stream;
+	std::string inputString(input[0]);
+	
+
+	return request;
+
+}
+
+Transfer::TransferRequest Transfer::getTransferRequest(std::istream* input)
+{
+	return {};
+}
+
+bool Transfer::isEmpty(Transfer::TransferRequest request)
+{
+	if(!request.ipAdress.empty() ||
+		 !request.file.empty())
+	{
+			return true;
+	}
+	return false;
+
+}
+
+
+
+
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo Starting to build...
 if [ ! -d "build/" ]; then
   mkdir build/
 fi
-clang++  Main.cpp -o build/Transfer
+clang++  Main.cpp -o build/Transfer -std=c++11
 chmod 755 build/Transfer
 
 echo ... build finished!
