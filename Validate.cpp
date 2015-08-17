99ad60c7e238722a450c98a23138671c1b971bed
DurRean <rick.herstol@gmail.com>
2015-08-17 15:05:00 +0200

    Check if path to filename is directory or a regular file

--- Validate.cpp
+++ Validate.cpp
@@ -1,5 +1,6 @@
 #include "Validate"
 
+
 // TODO BA Check if the Ip Adress is valid. Maybe with REGEX?
 bool Validate::ipAdress(std::string adress)
 {
@@ -9,5 +10,24 @@ bool Validate::ipAdress(std::string adress)
 // TODO BA Check if the filename is a valid path and a valid file
 bool Validate::file(std::string fileName)
 {
-    return true;
+    struct stat s;
+    int status;
+
+    status = stat(fileName.c_str(), &s);
+
+    if(status != 0) {
+        printf ("Error with stat(), errno = %d\n", errno);
+        return false;
+    }
+
+    if (S_ISREG (s.st_mode)) {
+        //printf ("%s is a regular file.\n", fileName.c_str());
+        return true;
+    }
+    if (S_ISDIR (s.st_mode)) {
+        printf ("Error, %s is a directory.\n", fileName.c_str());
+        return false;
+    }
+
+    return false;
 }
