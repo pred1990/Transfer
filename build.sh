01539103ec1152e5d5e7dcdd6e2963b538aee3ae
pred1990 <steffenkin@hotmail.de>
2015-08-27 23:53:14 +0200

    fixed build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo \#\#\# Starting to build...
 if [ ! -d "build/" ]; then
   mkdir build/
 fi
-clang++ Main.cpp Validate.cpp Util.cpp NetworkLnx.cpp -o build/Transfer -std=c++11 -pedantic -wall
+clang++ Main.cpp Validate.cpp Util.cpp NetworkLnx.cpp -o build/Transfer -std=c++11 -pedantic -Wall
 chmod 755 build/Transfer
 
 echo \#\#\# ...build finished!
