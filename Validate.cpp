18979820fecbce734028dad1886f44bb8f190106
Sebastian <sxw@tardis.fritz.box>
2015-08-27 22:49:18 +0200

    Validate File

--- Validate.cpp
+++ Validate.cpp
@@ -10,24 +10,26 @@ bool Validate::ipAdress(std::string adress)
 // TODO BA Check if the filename is a valid path and a valid file
 bool Validate::file(std::string fileName)
 {
-    struct stat s;
-    int status;
+    // Test file/folder with access function
+    return (access(fileName.c_str()) != -1);
+//    struct stat s;
+//    int status;
 
-    status = stat(fileName.c_str(), &s);
+//    status = stat(fileName.c_str(), &s);
 
-    if(status != 0) {
-        printf ("Error with stat(), errno = %d\n", errno);
-        return false;
-    }
+//    if(status != 0) {
+//        printf ("Error with stat(), errno = %d\n", errno);
+//        return false;
+//    }
 
-    if (S_ISREG (s.st_mode)) {
-        //printf ("%s is a regular file.\n", fileName.c_str());
-        return true;
-    }
-    if (S_ISDIR (s.st_mode)) {
-        printf ("Error, %s is a directory.\n", fileName.c_str());
-        return false;
-    }
+//    if (S_ISREG (s.st_mode)) {
+//        //printf ("%s is a regular file.\n", fileName.c_str());
+//        return true;
+//    }
+//    if (S_ISDIR (s.st_mode)) {
+//        printf ("Error, %s is a directory.\n", fileName.c_str());
+//        return false;
+//    }
 
-    return false;
+//    return false;
 }
